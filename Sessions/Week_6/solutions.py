803c48a582da1140b2238c06589f7b3aa5bcc3b1
EddBr <35238776+EddBr@users.noreply.github.com>
2022-01-25 21:20:49 +0000

    Create solutions.py

---
+++ Sessions/Week_6/solutions.py
@@ -0,0 +1,9 @@
+#Challenge 1
+rows = int(input("Enter number of rows: ")
+for i in range(1,rows):
+           print("*"*rows)
+           
+#Challenge 2
+rows = int(input("Enter number of rows: ")
+for i in range(1,rows):
+           print(" "*(rows-i) + "*"*(2*i-1))
